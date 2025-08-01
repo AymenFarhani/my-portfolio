e7d7e0178f9725c4af8c2ceec6f64382cf3f1c27
Aymen Farhani <aymenfarhani28@gmail.com>
2025-08-01 15:41:00 +0200

    fix flags bug

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  imports: [CommonModule]
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+  phoneNumber = '+32 490 12 57 23';
+  email = 'aymenfarhani28@gmail.com';
   socialLinks = [
     { icon: 'bi-linkedin', url: 'https://linkedin.com/in/aymen-farhani' },
     { icon: 'bi-github', url: 'https://github.com/AymenFarhani' },
